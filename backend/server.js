8e9f24d6bd34a148e4226cff6cc372ad1a494fbe
Tanuja Kamble <91514738+Tanuja-Kamble@users.noreply.github.com>
2026-03-06 00:41:56 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.post("/login", (req, res) => {
 
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
